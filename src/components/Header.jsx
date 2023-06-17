6f6d92684cf0299db9b50dc3613ff4bb9bf6b1e4
@quannd <quannd@vmodev.com>
2023-06-17 14:44:49 +0700

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
---
+++ src/routers/index.jsx
@@ -0,0 +1,5 @@
+const publicRoutes = [];
+
+const privateRoutes = [];
+
+export default { publicRoutes, privateRoutes };
